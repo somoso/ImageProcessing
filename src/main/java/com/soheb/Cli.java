d596bdb13339c1823b1db4e8c3d0d005edd187cb
Soheb <soheby@gmail.com>
2015-06-28 16:37:04 +0100

    added help

--- src/main/java/com/soheb/Cli.java
+++ src/main/java/com/soheb/Cli.java
@@ -9,16 +9,22 @@ public class Cli {
 
     private boolean loggingAllowed;
     private boolean parallel;
+    private boolean showHelp;
 
     public Cli(String[] args) {
         loggingAllowed = false;
         parallel = true;
         Options options = new Options();
         options.addOption("v", false, "Verbose (Enable logging output)");
-        options.addOption("s", true, "Enable single processing of jobs");
+        options.addOption("s", false, "Enable single processing of jobs");
+        options.addOption("h", "help", false, "Show help");
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine line = parser.parse(options, args);
+            if (line.hasOption("h")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("Main", options);
+            }
             loggingAllowed = line.hasOption("v");
             parallel = !line.hasOption("s");
         } catch (ParseException e) {
