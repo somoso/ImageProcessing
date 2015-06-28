fafc9608babb7e63deb8cad9f4f25d995f09d66e
Soheb <soheby@gmail.com>
2015-06-28 16:30:14 +0100

    First commit of Image Processing code

---
+++ src/main/java/com/soheb/Cli.java
@@ -0,0 +1,38 @@
+package com.soheb;
+
+import org.apache.commons.cli.*;
+
+/**
+ * Created by soheb on 28/06/2015.
+ */
+public class Cli {
+
+    private Options options;
+
+    private boolean loggingAllowed;
+    private boolean parallel;
+
+    public Cli(String[] args) {
+        loggingAllowed = false;
+        parallel = true;
+        options = new Options();
+        options.addOption("v", false, "Verbose (Enable logging output)");
+        options.addOption("s", true, "Enable single processing of jobs");
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine line = parser.parse(options, args);
+            loggingAllowed = line.hasOption("v");
+            parallel = !line.hasOption("s");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isLoggingAllowed() {
+        return loggingAllowed;
+    }
+
+    public boolean isParallel() {
+        return parallel;
+    }
+}
---
+++ src/main/java/com/soheb/FrequentColourImage.java
@@ -0,0 +1,68 @@
+package com.soheb;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Created by soheb on 28/06/2015.
+ */
+public class FrequentColourImage {
+
+    public static String getFrequentColourHex(File file) throws IOException {
+        BufferedImage image = ImageIO.read(file);
+
+        int height = image.getHeight();
+        int width = image.getWidth();
+
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rgb = image.getRGB(i, j);
+                int[] rgbArr = getRGBArr(rgb);
+                // Filter out grays....
+                if (!isGray(rgbArr)) {
+                    Integer counter = m.get(rgb);
+                    if (counter == null)
+                        counter = 0;
+                    counter++;
+                    m.put(rgb, counter);
+                }
+            }
+        }
+        return getMostCommonColour(m);
+    }
+
+    public static String getMostCommonColour(Map<Integer, Integer> map) {
+        List list = new LinkedList<>(map.entrySet());
+        Collections.sort(list, (o1, o2) -> ((Comparable) ((Map.Entry) (o1)).getValue())
+                .compareTo(((Map.Entry) (o2)).getValue()));
+        if (list.isEmpty()) return "No Colour Found";
+        Map.Entry me = (Map.Entry) list.get(list.size() - 1);
+        int[] rgb = getRGBArr((Integer) me.getKey());
+        return String.format("#%02x%02x%02x", rgb[0], rgb[1], rgb[2]);
+    }
+
+    public static int[] getRGBArr(int pixel) {
+        int alpha = (pixel >> 24) & 0xff;
+        int red = (pixel >> 16) & 0xff;
+        int green = (pixel >> 8) & 0xff;
+        int blue = (pixel) & 0xff;
+        return new int[]{red, green, blue};
+
+    }
+
+    public static boolean isGray(int[] rgbArr) {
+        int rgDiff = rgbArr[0] - rgbArr[1];
+        int rbDiff = rgbArr[0] - rgbArr[2];
+        // Filter out black, white and grays...... (tolerance within 10 pixels)
+        int tolerance = 10;
+        if (rgDiff > tolerance || rgDiff < -tolerance)
+            if (rbDiff > tolerance || rbDiff < -tolerance) {
+                return false;
+            }
+        return true;
+    }
+}
